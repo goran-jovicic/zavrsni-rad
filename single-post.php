f7c5025f7a2ce24826b342e08579ba9d0804b494
goranjovicic <gugacns94@gmail.com>
2019-04-16 19:42:34 +0200

    finished single post page in php, now to add comments

--- single-post.php
+++ single-post.php
@@ -1,14 +1,12 @@
 <?php
-    // ako su mysql username/password i ime baze na vasim racunarima drugaciji
-    // obavezno ih ovde zamenite
+
     $servername = "127.0.0.1";
     $username = "root";
-    $password = "root";
-    $dbname = "vivify_blog_3_dan";
+    $password = "vivify";
+    $dbname = "blog";
 
     try {
         $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        // set the PDO error mode to exception
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch(PDOException $e)
@@ -21,118 +19,69 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
 
-    <link rel="shortcut icon" href="favicon.ico">
-    <title>Vivify Academy Blog - Homepage</title>
+    <title>Vivify Blog</title>
 
-    <link rel="stylesheet" href="css/styles.css">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="styles/blog.css" rel="stylesheet">
+    <link href="styles/styles.css" rel="stylesheet">
 </head>
-<body class="va-l-page va-l-page--single">
+<body>
 
-<?php include('header.php'); ?>
+<?php include('php/header.php'); ?>
+    <main role="main" class="container">
 
-    <div class="va-l-container">
-        <main class="va-l-page-content">
+        <?php
+            if (isset($_GET['post_id'])) {
 
-            <?php
-                if (isset($_GET['post_id'])) {
+                $sql = "SELECT * FROM posts WHERE posts.id = {$_GET['post_id']}";
+                $statement = $connection->prepare($sql);
 
-                    // pripremamo upit
-                    $sql = "SELECT id, title, created_at, content, created_by FROM posts WHERE posts.id = {$_GET['post_id']}";
-                    $statement = $connection->prepare($sql);
+                $statement->execute();
 
-                    // izvrsavamo upit
-                    $statement->execute();
+                $statement->setFetchMode(PDO::FETCH_ASSOC);
 
-                    // zelimo da se rezultat vrati kao asocijativni niz.
-                    // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
-                    $statement->setFetchMode(PDO::FETCH_ASSOC);
+                $singlePost = $statement->fetch();
 
-                    // punimo promenjivu sa rezultatom upita
-                    $singlePost = $statement->fetch();
+        ?>
 
-                    // koristite var_dump kada god treba da proverite sadrzaj neke promenjive
-                        echo '<pre>';
-                        var_dump($singlePost);
-                        echo '</pre>';
-
-            ?>
-
-                    <article class="va-c-article">
-                        <header>
-                            <h1><?php echo $singlePost['title'] ?></h1>
-
-                            <!-- zameniti privremenu kategoriju pravom kategorijom blog post-a iz baze -->
-                            <h3>category: <strong>Sports</strong></h3>
-
-                            <!-- zameniti  datum i ime sa pravim imenom i datumom blog post-a iz baze -->
-                            <div class="va-c-article__meta">12.06.2017. by John Doe</div>
-                        </header>
-
-                        <!-- zameniti ovaj privremeni (testni) text sa pravim sadrzajem blog post-a iz baze -->
-                        <div>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt vitae molestias rem
-                                repellendus commodi provident? Magnam, nobis quisquam perferendis consectetur deserunt
-                                laboriosam pariatur a, eum suscipit ratione iusto ullam aperiam quas quod culpa dolore
-                                corrupti voluptatem placeat enim commodi in.</p>
-                            <p>Vel quasi sunt rem unde ea repellat eveniet at officia totam. Provident ut harum
-                                temporibus impedit odio quam amet accusamus ad quisquam velit incidunt praesentium
-                                cupiditate consectetur repellendus, fugiat quidem, officiis laudantium autem possimus
-                                ullam minima adipisci itaque? Eos, minus!</p>
-                            <p>Veritatis exercitationem enim magnam deserunt velit facere quos ea hic quibusdam
-                                molestiae minus, earum reprehenderit error architecto cum cumque perferendis quas
-                                impedit rerum sapiente facilis debitis! Error, obcaecati ea illum beatae voluptate
-                                consequatur, iusto quam sapiente fugiat, exercitationem maiores similique?</p>
-                            <p>Magni provident ex, doloribus architecto labore corrupti numquam. Beatae cumque alias
-                                aliquam iste ratione dolore in, odio libero numquam nemo reprehenderit modi magnam a
-                                laboriosam, ab quidem itaque deserunt explicabo facere deleniti illum, fuga vitae.
-                                Officiis at laborum doloremque assumenda.</p>
-                        </div>
-
-                        <footer>
-                            <h3>tags:
-
-                                <!-- zameniti testne tagove sa pravim tagovima blog post-a iz baze -->
-                                <a>football</a>, <a>champions league</a>, <a>qualifiers</a>
-                            </h3>
-                        </footer>
-
-                        <div class="comments">
-                            <h3>comments</h3>
-
-                            <!-- zameniti testne komentare sa pravim komentarima koji pripadaju blog post-u iz baze -->
-                            <div class="single-comment">
-                                <div>posted by: <strong>Pera Peric</strong> on 15.06.2017.</div>
-                                <div>Provident ut harum temporibus impedit odio quam amet accusamus ad quisquam velit
-                                    incidunt praesentium cupiditate consectetur repellendus, fugiat quidem, officiis
-                                    laudantium autem possimus ullam minima adipisci itaque? Eos, minus!
-                                </div>
-                            </div>
-                            <div class="single-comment">
-                                <div>posted by: <strong>Mitar Miric</strong> on 18.06.2017.</div>
-                                <div>Incidunt praesentium cupiditate consectetur repellendus, fugiat quidem, officiis
-                                    laudantium autem possimus ullam minima adipisci itaque? Eos, minus!
-                                </div>
-                            </div>
-                            <div class="single-comment">
-                                <div>posted by: <strong>Dule Savic</strong> on 20.06.2017.</div>
-                                <div>Jedna je Crvena Zvezda!</div>
-                            </div>
-                        </div>
-                    </article>
-
-            <?php
-                } else {
-                    echo('post_id nije prosledjen kroz $_GET');
-                }
-            ?>
-
-        </main>
+<!-- <div class="blog-post">
+        <h2 class="blog-post-title"><a href="single-post.php?post_id=<?php echo($post['id']) ?>"><?php echo($post['title']) ?></a></h2>
+        <p class="blog-post-meta"><?php echo($post['created_at']) ?> by <?php echo($post['author']) ?></p>
+        <p><?php echo($post['body']) ?></p>
+    </div> -->
+<div class="row">
+    <div class="col-sm-8 blog-main">
+        <div class="blog-post">
+            <header>
+                <h2 class="blog-post-title"><?php echo $singlePost['title'] ?></h2>
+                <p class="blog-post-meta"><?php echo($singlePost['created_at']) ?> by <?php echo($singlePost['author']) ?></p>
+            </header>
+            <p><?php echo($singlePost['body']) ?></p>
+        </div>
+        <!-- <nav class="blog-pagination">
+            <a class="btn btn-outline-primary" href="#">Older</a>
+            <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+        </nav> -->
     </div>
+<?php include('php/sidebar.php');?>
 
-    <?php include('footer.php'); ?>
+<?php
+    } else {
+    echo('post_id nije prosledjen kroz $_GET');
+    }
+?>
+    </div>
+</main>
+
+<?php include('php/footer.php'); ?>
 
 </body>
 </html>
